77332fb3e2e0bdb52f07db76d50fb4a13cab4331
Leandro Lago da Silva <leandro@datacampo.com.br>
2022-02-18 16:35:10 -0300

    github oauth finished

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,52 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb';
+import { isPropertyAccessChain } from "typescript";
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      params: { scope: 'read:user,user:email' }
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    
+    async signIn({ user, account, profile }) {
+      const {email} = user 
+      
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
