039ca1c43a5ed963472176e037374d837d76b12a
Leandro Lago da Silva <leandro@datacampo.com.br>
2022-06-29 17:33:16 -0300

    firsts tests

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  testIgnorePatterns: ["/node_modules/", "/.next/"],
+  verbose: true,
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   setupFilesAfterEnv: [
     "<rootDir>/src/tests/setupTests.ts"
   ],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "/node_modules/babel-jest"
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
   },
   testEnvironment: 'jsdom'
 }
