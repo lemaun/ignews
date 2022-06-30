d9b22624e6e200fb9f87ae50dc0c618108758c3b
Leandro Lago da Silva <leandro@datacampo.com.br>
2022-06-30 18:26:11 -0300

    add subscribe tests

---
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -0,0 +1,65 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { SubscribeButton } from '.'
+
+jest.mock('next-auth/react')
+jest.mock('next/router')
+
+describe('SubscribeButton Component', () => {
+  
+  it('renders correctly', () => {
+    const useSessionMocked = jest.mocked(useSession)
+
+    useSessionMocked.mockReturnValueOnce({data: null, status: "loading"})
+
+    render(<SubscribeButton/>)
+
+    expect(screen.getByText('Subscribe now')).toBeInTheDocument()
+    
+  })
+
+  it("redirects user to sign in when NOT authenticated", () => {
+    const signInMocked = jest.mocked(signIn)
+    const useSessionMocked = jest.mocked(useSession)
+
+    useSessionMocked.mockReturnValueOnce({ data: null, status: "loading" })
+
+    render(<SubscribeButton />)
+
+    const subscribeButton = screen.getByText(/subscribe now/i)
+
+    fireEvent.click(subscribeButton)
+
+    expect(signInMocked).toHaveBeenCalled()
+  });
+  
+  it('redirects to posts when user already has a subscription', () => {
+    const useRouterMocked = jest.mocked(useRouter)
+    const useSessionMocked = jest.mocked(useSession)
+    const pushMock = jest.fn()
+
+    useSessionMocked.mockReturnValueOnce({
+      data: {
+        user: { name: "John Doe", email: "john.doe@example.com" },
+        expires: "fake-expires",
+        activeSubscription: "fake-active-subscription",
+      },
+      status: "authenticated",
+    });
+
+    useRouterMocked.mockReturnValueOnce({
+      push: pushMock,
+    } as any)
+    
+    render(<SubscribeButton />)
+    const subscribeButton = screen.getByText(/Subscribe now/i)
+  
+    fireEvent.click(subscribeButton)
+  
+    expect(pushMock).toHaveBeenCalled()
+    expect(pushMock).toHaveBeenCalledWith("/posts")
+    
+  })
+
+})
